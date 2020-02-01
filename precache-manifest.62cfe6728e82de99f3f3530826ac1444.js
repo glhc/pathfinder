393eacf7b8beecc78d918a4d15480bfe7732ca0d
Patrick Horne <patrickahorne@gmail.com>
2020-02-01 16:19:32 +1100

    Updates

---
+++ precache-manifest.62cfe6728e82de99f3f3530826ac1444.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "476210929449ea14ed37c3f5cb6aa9f2",
+    "url": "/glhc/pathfinder/index.html"
+  },
+  {
+    "revision": "194de2d3d83a7495811b",
+    "url": "/glhc/pathfinder/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "7bcee494daa4dbeeb2ac",
+    "url": "/glhc/pathfinder/static/js/2.eff90442.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/glhc/pathfinder/static/js/2.eff90442.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "194de2d3d83a7495811b",
+    "url": "/glhc/pathfinder/static/js/main.6453cdb1.chunk.js"
+  },
+  {
+    "revision": "9b031c72e399b59850e3",
+    "url": "/glhc/pathfinder/static/js/runtime-main.5cd19ed0.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/glhc/pathfinder/static/media/logo.5d5d9eef.svg"
+  }
+]);
