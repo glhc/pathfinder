f21840c8b89e83ceeb90b7e17a6f439afd4aa708
Patrick Horne <patrickahorne@gmail.com>
2020-02-04 01:14:41 +1100

    Migrate everything, still bugs in Node.js

---
+++ src/Node.js
@@ -0,0 +1,79 @@
+'use strict';
+
+/**
+ * Creates a Start Node or the End Node
+ * @constructor
+ * @param xPos {Number} - The x position
+ * @param yPos {Number} - The y position
+ * @returns {Object} - A node.
+ */
+export default function Node(xPos = 10, yPos = 10) {
+  /** the x-position of the node */
+  this.xPos = xPos;
+  /** the y-position of the node */
+  this.yPos = yPos;
+
+  /**
+   * Move up.
+   * @returns {Number|Null} - y-position if successful, null otherwise
+   */
+  this.up = () => {
+    if (yPos - 1 < 0) {
+      return null;
+    } else {
+      this.yPos -= 1;
+      return this.yPos;
+    }
+  };
+
+  /**
+   * Move down.
+   * @returns {Number|Null} - y-position if successful, null otherwise
+   */
+  this.down = () => {
+    if (yPos + 1 < rows) {
+      return null;
+    } else {
+      this.yPos += 1;
+      return this.xPos;
+    }
+  };
+  /**
+   * Move left.
+   * @returns {Number|Null} - y-position if successful, null otherwise
+   */
+  this.left = () => {
+    if (xPos - 1 < 0) {
+      return null;
+    } else {
+      this.xPos -= 1;
+      return this.xPos;
+    }
+  };
+  /**
+   * Move right.
+   * @returns {Number|Null} - x-position if successful, null otherwise
+   */
+  this.right = () => {
+    if (xPos + 1 < columns) {
+      return null;
+    } else {
+      this.xPos += 1;
+      return this.xPos;
+    }
+  };
+
+  /**
+   * Searches an adjacent or diagonally adjacent square, marking as visited
+   * @param {number} x - the x-position to search
+   * @param {number} y - the y-position to search
+   * @returns {null} - returns null if the peek is invalid
+   */
+  this.peek = (x, y) => {
+    if (this.xPos - 1 < x  || x < this.xPos + 1) {
+      return null;
+    } else {
+      gridData[y][x].visited = true;
+    }
+  };
+};
--- src/index.js
+++ src/index.js
@@ -1,12 +1,81 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+"use strict";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import Node from './Node';
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+
+/**
+ * Generate a grid structure
+ * @returns {Array} - The grid data
+ */
+function gridData() {
+  let data = [];
+  let rows = 32;
+  let columns = 32;
+  let width = window.innerWidth / columns;
+  let height = width;
+
+  // TODO: get cell width to round up if theres leftover width
+
+  console.table({
+    width: width,
+    "total width / cell width": width / window.innerWidth
+  });
+
+  let xPos = 0;
+  let yPos = 0;
+
+  for (let row = 0; row < rows; row++) {
+    data.push([]);
+    for (let column = 0; column < columns; column++) {
+      data[row].push({
+        x: xPos,
+        y: yPos,
+        height: height,
+        width: width,
+        visited: false,
+        occupied: true
+      });
+      xPos += width;
+    }
+    xPos = 0;
+    yPos += height;
+  }
+  return data;
+}
+
+const gridDatastore = gridData();
+
+let grid = d3
+  .select("#grid")
+  .append("svg")
+  .attr("width", `${window.innerWidth}px`)
+  .attr("height", `${window.innerHeight}px`);
+
+let row = grid
+  .selectAll(".row")
+  .data(gridDatastore)
+  .enter()
+  .append("g")
+  .attr("class", "row");
+
+let column = row
+  .selectAll(".cell")
+  .data(d => d)
+  .enter()
+  .append("rect")
+  .attr("class", "cell")
+  .attr("x", d => d.x)
+  .attr("y", d => d.y)
+  .attr("height", d => d.height)
+  .attr("width", d => d.width)
+  .style("stroke", "#222")
+  .style("fill", "#fff");
+
+const Marker = new Node();
+const Goal = new Node();
+
+for (let i = 0; i < 80; i++) {
+  Marker.down();
+  console.table(Marker.xPos, Marker.yPos);
+};
+
