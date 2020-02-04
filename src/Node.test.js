bd2c2769924c0d23f0f10fade5d625e38a4ec4af
Patrick Horne <patrickahorne@gmail.com>
2020-02-05 00:54:48 +1100

    Write more tests

--- src/Node.test.js
+++ src/Node.test.js
@@ -23,7 +23,7 @@ describe("Node", () => {
   });
 
   describe("can move up", () => {
-    test("by 1", () => {
+    test("by 1 space", () => {
       const testGridData = GridData();
       const TestNode = new Node(testGridData);
       const start = {
@@ -44,6 +44,81 @@ describe("Node", () => {
       };
       TestNode.up(yDistance);
       expect(TestNode.yPos).toBe(start.y - yDistance);
+    });
+  });
+
+  describe("can move down", () => {
+    test("by 1 space", () => {
+      const testGridData = GridData();
+      const TestNode = new Node(testGridData);
+      const start = {
+        x: TestNode.xPos,
+        y: TestNode.yPos
+      };
+      TestNode.down();
+      expect(TestNode.yPos).toBe(start.y + 1);
+    });
+
+    test("by several spaces", () => {
+      const yDistance = faker.random.number({min: 2, max: 10});
+      const testGridData = GridData();
+      const TestNode = new Node(testGridData);
+      const start = {
+        x: TestNode.xPos,
+        y: TestNode.yPos
+      };
+      TestNode.up(yDistance);
+      expect(TestNode.yPos).toBe(start.y + yDistance);
+    });
+  });
+
+  describe("can move left", () => {
+    test("by 1 space", () => {
+      const testGridData = GridData();
+      const TestNode = new Node(testGridData);
+      const start = {
+        x: TestNode.xPos,
+        y: TestNode.yPos
+      };
+      TestNode.up();
+      expect(TestNode.zPos).toBe(start.x - 1);
+    });
+
+    test("by several spaces", () => {
+      const xDistance = faker.random.number({min: 2, max: 10});
+      const testGridData = GridData();
+      const TestNode = new Node(testGridData);
+      const start = {
+        x: TestNode.xPos,
+        y: TestNode.yPos
+      };
+      TestNode.left(xDistance);
+      expect(TestNode.xPos).toBe(start.x - xDistance);
+    });
+  });
+
+  describe("can move right", () => {
+    test("by 1 space", () => {
+      const testGridData = GridData();
+      const TestNode = new Node(testGridData);
+      const start = {
+        x: TestNode.xPos,
+        y: TestNode.yPos
+      };
+      TestNode.right();
+      expect(TestNode.xPos).toBe(start.x + 1);
+    });
+
+    test("by several spaces", () => {
+      const xDistance = faker.random.number({min: 2, max: 10});
+      const testGridData = GridData();
+      const TestNode = new Node(testGridData);
+      const start = {
+        x: TestNode.xPos,
+        y: TestNode.yPos
+      };
+      TestNode.up(xDistance);
+      expect(TestNode.xPos).toBe(start.x + yDistance);
     });
   });
 });
