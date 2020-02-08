9fcdb96ed1f554eeacb0a74e30185895815b81ba
Patrick Horne <patrickahorne@gmail.com>
2020-02-08 22:55:00 +1100

    Build out map components

--- src/map/Graph.js
+++ src/map/Graph.js
@@ -14,7 +14,7 @@ function Graph() {
    * Helper method specific to this 2d map-type implementation
    * @private
    */
-  const distanceBetweenNodes = function(sourceNode, targetNode) {
+  const distanceBetweenNodes = function (sourceNode, targetNode) {
     let xDist = targetNode.xPos - sourceNode.xPos;
     let yDist = targetNode.yPos - sourceNode.yPos;
 
@@ -32,7 +32,7 @@ function Graph() {
    * Adds an edge between two nodes
    * @method
    */
-  this.addEdge = function(sourceNode, targetNode, cost) {
+  this.addEdge = function (sourceNode, targetNode, oneDirection, cost) {
     // Figure out the cost if it wasn't passed into this function
     if (cost == undefined) {
       cost = calculateCost(sourceNode, targetNode);
@@ -42,7 +42,8 @@ function Graph() {
     sourceNode.edges.push(new Edge(sourceNode, targetNode, cost));
 
     //add the edge to the target node
-    targetNode.edges.push(new Edge(targetNode, sourceNode, cost));
+    if (!oneDirection)
+      targetNode.edges.push(new Edge(targetNode, sourceNode, cost));
   };
 
   /**
@@ -50,8 +51,9 @@ function Graph() {
    * @method
    * @param {Number} xPos - The x-position of the node
    * @param {Number} yPos - The y-position of the node.
+   * @todo Check for pre-existing node before adding another identical one
    */
-  this.addNode = function(xPos, yPos) {
+  this.addNode = function (xPos, yPos) {
     let node = new GraphNode(xPos, yPos);
     this.nodes.push(node)
   };
@@ -62,20 +64,33 @@ function Graph() {
    * @param {Number} costThreshold - The maximum for which
    * nodes will be connected
    */
-  this.connectNearbyNodes = function(costThreshold) {
+  this.connectNearbyNodes = function (costThreshold) {
     for (node of this.nodes) {
-      
       // create a list of nodes that are within or equal to the cost threshold
-      let neighbourNodes = this.nodes.filter(candidate => {
-        return calculateCost(candidate) <= costThreshold;
+      this.nodes.forEach(candidate => {
+        if (candidate !== node) {
+          let cost = calculateCost(node, candidate);
+          if (cost <= costThreshold) {
+            this.addEdge(node, candidate, true, cost);
+          }
+        }
       });
-
-      // Add edges between these nodes
-      for (validTargetNode of neighbourNodes) {
-        this.addEdge(node, validTargetNode);
-      }
     }
   };
+
+  /**
+   * finds a node based on the co-ords provided
+   * @param {Number} x - The x-position of the node
+   * @param {Number} y - The y-position of the node
+   * @returns {(Node|null)} - Node if exists
+   */
+  this.findNodesByData = function (x, y, radius = 1) {
+    return this.nodes.filter(node => {
+      let xDist = x - node.xPos;
+      let yDist = y - node.yPos;
+      return Math.sqrt(xDist * xDist + yDist * yDist) <= radius;
+    });
+  }
 
   /**
    * @member {Array.<GraphNodes>} nodes - A list of nodes
