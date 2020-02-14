579332a827f7bc3bf8e252b79879eadfad60bf97
Patrick Horne <patrickahorne@gmail.com>
2020-02-15 01:04:21 +1100

    Updates

--- precache-manifest.a556ad7b3d417e2bec534ec4d80774dd.js
+++ precache-manifest.a556ad7b3d417e2bec534ec4d80774dd.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "0d3ad707e79757dfb2a189c0ad11a15e",
+    "revision": "5381aa9748765aa0bce96520017b1c6a",
     "url": "/pathfinder/index.html"
   },
   {
-    "revision": "7e5d33f3ca0165c43bf4",
+    "revision": "ddeab44291b5a20282b4",
     "url": "/pathfinder/static/css/main.e508286b.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/pathfinder/static/js/2.815492ae.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "7e5d33f3ca0165c43bf4",
-    "url": "/pathfinder/static/js/main.92405de0.chunk.js"
+    "revision": "ddeab44291b5a20282b4",
+    "url": "/pathfinder/static/js/main.b8d1c9d4.chunk.js"
   },
   {
     "revision": "130ba9a6e28bde6d8d17",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/pathfinder/precache-manifest.c7859b1c40e0799687a9b53011d48938.js"
+  "/pathfinder/precache-manifest.a556ad7b3d417e2bec534ec4d80774dd.js"
 );
 
 self.addEventListener('message', (event) => {
